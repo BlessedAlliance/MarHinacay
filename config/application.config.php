82ca9dc7ca2804990998ca1240534d2291e78901
gabbydgab <gamba@gabbydgab.com>
2016-06-23 23:09:03 +0800

    fixed layout

--- config/application.config.php
+++ config/application.config.php
@@ -2,11 +2,12 @@
 
 return [
     // This should be an array of module namespaces used in the application.
-    'modules' => [
+    'modules' => [        
         'Zend\Router',
         'Zend\Validator',
         'Site',
-        'AimWorld', 
+        'AimWorld',
+        'BlessedAlliance',
         'Application'
     ],
     // These are various options for the listeners attached to the ModuleManager
